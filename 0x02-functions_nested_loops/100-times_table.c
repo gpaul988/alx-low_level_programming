7ec1634b03f24d6c939f63c3bd20f41aca918328
gpaul988 <gpaul988@gmail.com>
2023-03-21 07:41:13 -0700

    final commit for functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,12 @@ if (n >= 0 && n <= 15)
 for (num = 0; num <= n; num++)
 {
 _putchar('0');
-for(mult = 1; mult <= n; mult++)
+
+for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
+
 prod = num * mult;
 if (prod <= 99)
 _putchar(' ');
