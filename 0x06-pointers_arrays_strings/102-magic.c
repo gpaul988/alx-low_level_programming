4d87fe517f89521cea707e5bcc711c9a382747d7
gpaul988 <gpaul988@gmail.com>
2023-03-30 10:19:15 -0700

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-int main(void){
-  int n;
-  int a[5];
-  int *p;
+int main(void)
+{
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
-
- /*
-  * adding any of these lines makes code to print
-  *  "a[2] = 98" at output
-  */
-  p[5] = 98; //OR *(p + 5) = 98;
-
-  printf("a[2] = %d\n", a[2]); //Prints a[2] = 98
-  return (0);
+a[2] = 1024;
+p = &n;
+/*
+ * write your lineof code here...
+ * Remember:
+ * - you are not allowed touse a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+printf("n    = %p\n", &n);
+printf("a[0] = %p\n", &a[0]);
+printf("a[1] = %p\n", &a[1]);
+printf("a[2] = %p\n", &a[2]);
+printf("p    = %p\n", (p + 5));
+printf("a[3] = %p\n", &a[3]);
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
