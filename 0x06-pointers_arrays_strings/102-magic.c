8d145d467f28f9e4d2dc8eda7f1537de8f9898c4
gpaul988 <gpaul988@gmail.com>
2023-03-30 17:43:48 -0700

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,21 +9,15 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
- * write your lineof code here...
- * Remember:
- * - you are not allowed touse a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
-printf("n    = %p\n", &n);
-printf("a[0] = %p\n", &a[0]);
-printf("a[1] = %p\n", &a[1]);
-printf("a[2] = %p\n", &a[2]);
-printf("p    = %p\n", (p + 5));
-printf("a[3] = %p\n", &a[3]);
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+printf("a[2] = %p\n", a[2]);
 return (0);
 }
