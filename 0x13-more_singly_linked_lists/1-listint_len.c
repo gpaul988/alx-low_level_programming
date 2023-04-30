3c8d262fe17494e709990848693c76870146dd32
gpaul988 <gpaul988@gmail.com>
2023-04-30 07:33:33 -0700

    More Singly

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,18 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - Linked list returns number of elements
+ * @h: linked list of the type listint_t to traverse
  *
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
-
-	while (h)
-	{
-		num++;
-		h = h->next;
-	}
-
-	return (num);
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+}
+return (num);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,15 @@
 #include "lists.h"
-
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_listint - frees linked lists
+ * @head: listint_t list that will be freed
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
