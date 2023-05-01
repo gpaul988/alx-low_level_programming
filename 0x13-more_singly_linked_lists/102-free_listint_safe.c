3ca207b84974426fe798e388468e9ed4cb593e1f
gpaul988 <gpaul988@gmail.com>
2023-05-01 16:40:10 -0700

    more singly

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,83 +1,36 @@
 #include "lists.h"
-
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * looped_listint_count - Counts number of unique nodes in looped
- * listint_t linked list.
- * @head: Head of listint_t to check pointer.
- *
- * Return: If list not looped - 0.
- * Otherwise - number of unique nodes in list.
- */
-size_t looped_listint_count(listint_t *head)
-{
-	listint_t *tortoise, *hare;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-	tortoise = head->next;
-	hare = (head->next)->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-			return (nodes);
-		}
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
-	return (0);
-}
-
-/**
- * free_listint_safe - Frees listint_t list safely
- * @h: Head of listint_t list addresses pointer.
- *
- * Return: Size of list which was freed.
- *
- */
+* free_listint_safe - free linked list
+* @h: Pointer to first node in linked list
+*
+* Return: No. of elements in the freed list
+*/
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp;
-	size_t nodes, index;
+size_t len = 0;
+int diff;
+listint_t *temp;
 
-	nodes = looped_listint_count(*h);
-	if (nodes == 0)
-	{
-		for (; h != NULL && *h != NULL; nodes++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-	}
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-
-		*h = NULL;
-	}
-	h = NULL;
-	return (nodes);
+if (!h || !*h)
+return (0);
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len++;
+break;
+}
+}
+*h = NULL;
+return (len);
 }
