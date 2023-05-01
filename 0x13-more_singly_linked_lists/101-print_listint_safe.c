01190da8c50cfafe053e593b8b01a526f0c88853
gpaul988 <gpaul988@gmail.com>
2023-05-01 16:33:49 -0700

    more singly

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,22 +1,53 @@
 #include "lists.h"
 #include <stdio.h>
-size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
+size_t looped_listint_len(const listint_t *head);
 /**
- * looped_listint_len - Counts number of unique nodes
- * in looped listint_t linked list.
- * @head: A pointer to head of listint_t to check.
- *
- * Return: If list not looped - 0.
- * Otherwise -  number of unique nodes in list.
- */
+* print_listint_safe - prints listint_t list safely.
+* @head: pointer to head of listint_t list.
+*
+* Return: Number of nodes in list.
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nodes, index = 0;
+
+nodes = looped_listint_len(head);
+if (nodes == 0)
+{
+for (; head != NULL; nodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+return (nodes);
+}
+
+/**
+* looped_listint_len - Counts number of unique nodes in a
+* looped listint_t linked list.
+* @head: Pointer to head of listint_t to check.
+*
+* Return: If list is not looped - 0.
+* Otherwise - Number of unique nodes in list.
+*/
 size_t looped_listint_len(const listint_t *head)
 {
 const listint_t *tortoise, *hare;
 size_t nodes = 1;
+
 if (head == NULL || head->next == NULL)
 return (0);
-
 tortoise = head->next;
 hare = (head->next)->next;
 while (hare)
@@ -43,30 +74,4 @@ hare = (hare->next)->next;
 }
 return (0);
 }
-/**
- * print_listint_safe - Safely prints listint_t list.
- * @head: A pointer to head the listint_t list.
- *
- * Return: List of node numbers.
- */
-size_t print_listint_safe(const listint_t *head)
-{
-size_t nodes, index = 0;
-nodes = looped_listint_len(head);
-if (nodes == 0)
-{
-for (; head != NULL; nodes++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-}
-else
-{
-for (index = 0; index < nodes; index++)
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-printf("-> [%p] %d\n", (void *)head, head->n);
-return (nodes);
-}
+
