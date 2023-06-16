7ca73993207891357f4177880ddae66d13d5c80a
gpaul988 <gpaul988@gmail.com>
2023-06-15 17:05:43 -0700

    insert dnodeint fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,54 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index – Includes new node in a dlistint_t
-*                            list at given Location.
-* @h: Directs to the beginning of the dlistint_t list.
-* @idx: Location to include the new node.
-* @n: Integer for the latest node to hold.
+* insert_dnodeint_at_index - Includes new node at
+* given location
 *
-* Return: If purpose fails - NULL.
-*         O/W - Location of the latest node.
+* @h: Beginning of list
+* @idx: index of new node
+* @n: Worth of new node
+* Return: Locates new node, or NULL if failed
 */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *tmp = *h, *new;
+dlistint_t *new;
+dlistint_t *head;
+unsigned int i;
 
+new = NULL;
 if (idx == 0)
-return (add_dnodeint(h, n));
-
-for (; idx != 1; idx--)
+new = add_dnodeint(h, n);
+else
 {
-tmp = tmp->next;
-if (tmp == NULL)
-return (NULL);
-}
-
-if (tmp->next == NULL)
-return (add_dnodeint_end(h, n));
-
+head = *h;
+i = 1;
+if (head != NULL)
+while (head->prev != NULL)
+head = head->prev;
+while (head != NULL)
+{
+if (i == idx)
+{
+if (head->next == NULL)
+new = add_dnodeint_end(h, n);
+else
+{
 new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-return (NULL);
-
+if (new != NULL)
+{
 new->n = n;
-new->prev = tmp;
-new->next = tmp->next;
-tmp->next->prev = new;
-tmp->next = new;
-
+new->next = head->next;
+new->prev = head;
+head->next->prev = new;
+head->next = new;
+}
+}
+break;
+}
+head = head->next;
+i++;
+}
+}
 return (new);
 }
