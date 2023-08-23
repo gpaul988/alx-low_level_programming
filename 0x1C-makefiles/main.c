ca8e5edd4a7076bbf453f52d81ee8de8555ec5cb
gpaul988 <gpaul988@gmail.com>
2023-08-23 14:45:28 -0700

    Makefiles complete

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,14 @@
+#include "m.h"
+
+/**
+ * main - Begining of the code
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+/* School */
+/* School */
