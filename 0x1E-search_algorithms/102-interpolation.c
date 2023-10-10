968478ee32b2d8239ee573f2806351f8c8a76160
gpaul988 <gpaul988@gmail.com>
2023-10-10 16:22:52 -0700

    completed

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,46 +1,43 @@
 #include "search_algos.h"
 
 /**
- * print_i - The assisted func to pull idx each check
- * @array: The array
- * @i: The index checked
- * Graham S.Paul (102-interpolation.c)
- */
-void print_i(int *array, size_t i)
-{
-	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-}
-
-/**
- * interpolation_search - SImilar to binary search, but use idx based on formula
- * @array: The given array of ints
- * @size: The size of array
- * @value: The worth to search for
- * Return: The  index at which value's found
- */
+  * interpolation_search - Looks for the worth in a sorted array
+  * of integers using interpolation search.
+  * @array: Directs to first element of the array to search.
+  * @size: Number of elements in the array.
+  * @value: Worth to look for.
+  *
+  * Return: If worth is not present or the array is NULL, -1.
+  * OTW, first index where the value is located.
+  *
+  * Description: Pulls worth every time it is compared in the array..
+  * Graham S. Paul ( 102-interpolation.c)
+  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t l = 0;
-	size_t r = size - 1;
-	size_t pos;
+	size_t i, l, r;
 
 	if (array == NULL)
 		return (-1);
 
-	pos = l + (((double)(r - l) / (array[r] - array[l]))
-		   * (value - array[l]));
-	while (pos < size)
+	for (l = 0, r = size - 1; r >= l;)
 	{
-		print_i(array, pos);
-		if (array[pos] == value)
-			return (pos);
-		else if (array[pos] < value)
-			l = pos + 1;
+		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		else
-			r = pos - 1;
-		pos = l + (((double)(r - l) / (array[r] - array[l]))
-			   * (value - array[l]));
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
 	}
-	printf("Value checked array[%lu] is out of range\n", pos);
+
 	return (-1);
 }
